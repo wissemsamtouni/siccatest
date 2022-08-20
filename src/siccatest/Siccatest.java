988653d8d2a2b45c990b17e7d15eab234be928cb
Elife-Kef-020 <Elife-Kef-020@Elife.local>
2022-08-21 00:03:15 +0100

    scrum master

---
+++ src/siccatest/Siccatest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package siccatest;
+
+/**
+ *
+ * @author Elife-Kef-020
+ */
+public class Siccatest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
